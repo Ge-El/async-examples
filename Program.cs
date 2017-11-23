eabc55b0997d1d25992c44510ec2c124af4af7c7
George El-jaji <George.eljaji@gmail.com>
2017-11-23 13:15:53 +0100

    Added examples

--- Program.cs
+++ Program.cs
@@ -1,12 +1,88 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace async_examples
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = await CalculateResult();
+
+            Console.WriteLine(result);
         }
+
+        /// <summary>
+        /// Calls an async method returnes an active task, likley yet to complete
+        /// </summary>
+        /// <returns>
+        /// A task of type string
+        /// </returns>
+        static Task<string> GetHtml()
+        {
+            // Execution is synchronous here
+            var client = new HttpClient();
+
+            return client.GetStringAsync("http://www.dotnetfoundation.org");
+        }
+
+
+        /// <summary>
+        /// Calls an async method returnes an completet task
+        /// </summary>
+        /// <remarks>
+        /// The use of await keyword causes the method to return a newly created task object. 
+        /// The members of Task<T> enables the caller to monitor the progress of the task.
+        /// No thread waits for the request
+        /// </remarks>
+        /// A freshly created task object
+        /// <returns>
+        /// </returns>
+        static async Task<string> GetHtmlAsync()
+        {
+            // Execution is synchronous here
+            var client = new HttpClient();
+
+            return await client.GetStringAsync("http://www.dotnetfoundation.org");
+        }
+
+        /// <summary>
+        /// Calculates alot of hard shit.
+        /// </summary>
+        /// <returns>
+        ///    A task of type string
+        /// </returns>
+        static async Task<string> CalculateResult()
+        {
+            // This queues up the work on the threadpool.
+            var expensiveResultTask = Task.Run(async () =>
+            {
+                await Task.Delay(10000);
+
+                return "done";
+            });
+
+
+            Console.WriteLine("Doing shit while waiting for task");
+            // Note that at this point, you can do some other work concurrently,
+            // as CalculateResult() is still executing!
+
+            // Execution of CalculateResult is yielded here!
+            var result = await expensiveResultTask;
+
+            return result;
+        }
+
+        static async ValueTask<string> GetNameAsync()
+        {
+            await Task.Delay(3000);
+
+            return "George";
+        }
+
+
     }
+
+
 }
